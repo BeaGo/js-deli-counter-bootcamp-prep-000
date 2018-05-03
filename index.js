015a813864644459f6545840fabd07776b077ace
Bea Go <beanicolego@gmail.com>
2018-05-03 05:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
     } else if (lineLength > 1) {
       const arr = []
       for ( let i = 0; i < lineLength; i++) {
-    arr.push(`${katzDeliLine.indexOf(katzDeliLine[i])+1}. ${katzDeliLine[i]}`);
+    arr.push(`${i+1}. ${katzDeliLine[i]}`);
     } return "The line is currently: " + arr.join(', ')
   } else {
     return "The line is currently empty."
